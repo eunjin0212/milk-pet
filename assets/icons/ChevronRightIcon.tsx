0abac3982064df5949007e176f469f491e50f7c9
EunJin Kim <eunjin9639@gmail.com>
2025-09-11 20:38:29 -0400

    ChevronRightIcon 컴포넌트 추가

---
+++ assets/icons/ChevronRightIcon.tsx
@@ -0,0 +1,5 @@
+const ChevronRightIcon = () => (<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7 16L13 10L7 4" stroke="#A8A8A8" strokeWidth="1.5" />
+</svg>
+)
+export default ChevronRightIcon;
