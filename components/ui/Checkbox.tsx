fd0e5f1863ecf01b789aaae27372ff85fc1be813
EunJin Kim <eunjin9639@gmail.com>
2025-09-11 20:30:30 -0400

    CheckboxProps의 onToggle을 선택적으로 변경하고 스타일 개선

--- components/ui/Checkbox.tsx
+++ components/ui/Checkbox.tsx
@@ -4,7 +4,7 @@ import { Pressable, Text, View } from "react-native";
 
 interface CheckboxProps {
   checked: boolean;
-  onToggle: () => void;
+  onToggle?: () => void;
   label: string;
   required?: boolean;
   full?: boolean;
@@ -18,10 +18,10 @@ const Checkbox = ({ checked, onToggle, label, required, full }: CheckboxProps) =
       accessibilityState={{ checked }}
       className="inline-flex flex-row items-center justify-center gap-base"
     >
-      <View className={`inline-flex items-center justify-center w-6 h-6${full ? ' rounded-full' : ''}`}>
+      <View className={`inline-flex items-center justify-center w-6 h-6${full ? ` rounded-full border border-gray-light ${checked ? 'border-main bg-main' : ''}` : ''}`}>
         <CheckboxIcon className={`${full && checked ? 'text-white' : !full && checked ? 'text-main' : 'text-gray-light'}`} />
       </View>
-      <Text className='text-base font-medium leading-6 -tracking-tighter text-gray-dark'>{label}{required ? ' (필수)' : ''}</Text>
+      <Text className='text-base font-medium leading-6 ml-2.5 -tracking-tighter text-gray-dark'>{label}{required ? ' (필수)' : ''}</Text>
     </Pressable>
   );
 };
